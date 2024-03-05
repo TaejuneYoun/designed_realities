fc866ba08b5514f32641e140fc0c2b67ff31a2d5
Steve Youn <44358803+TaejuneYoun@users.noreply.github.com>
2024-03-05 21:47:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,12 +6,6 @@ import {
   CSS2DRenderer,
   CSS2DObject,
 } from "three/addons/renderers/CSS2DRenderer.js";
-import axios from 'axios';
-
-axios.defaults.baseURL = render.githubusercontent.com;
-axios.defaults.withCredentials = true;
-res.writeHead(200, { 'Access-Control-Allow-Origin': 'true' });
-res.writeHead(200, { 'Access-Control-Allow-Credentials': 'true' });
 
 let camera, scene, renderer;
 
