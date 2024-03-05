389248a32508f998df084e65326d7e5b3fe66324
Steve Youn <44358803+TaejuneYoun@users.noreply.github.com>
2024-03-05 20:15:56 +0900

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function init() {
     scene = new THREE.Scene();
 
     new RGBELoader()
-        .setPath( '../designed_realities/raw/main/exhibition/' )
+        .setPath( '../raw/main/exhibition/' )
         .load( 'studio_small_08_1k.hdr', function ( texture ) {
 
             texture.mapping = THREE.EquirectangularReflectionMapping;
@@ -34,7 +34,7 @@ function init() {
 
             // model
 
-            const loader = new GLTFLoader().setPath( '../designed_realities/raw/main/exhibition/' );
+            const loader = new GLTFLoader().setPath( '../raw/main/exhibition/' );
             loader.load( 'Test.glb', async function ( gltf ) {
                 const model = gltf.scene;
               
